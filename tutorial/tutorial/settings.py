c8737fcfca24026bd19e257203c90bbb2637cd38
tfarrell-vz <farrell@abiresearch.com>
2014-10-06 12:52:14 -0400

    Settings for django REST framework with the quickstart app

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'rest_framework'
-    'quickstart'
+    'rest_framework',
+    'quickstart',
 )
 
 MIDDLEWARE_CLASSES = (
--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from rest_framework import routers
-from tutorial.quickstart import views
+from quickstart import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
